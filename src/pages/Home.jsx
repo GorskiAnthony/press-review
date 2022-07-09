c165d90f6b70cdeed52904915b95ef87672eca84
Anthony Gorski <anthony.gorski@wildcodeschool.com>
2022-07-09 22:27:20 +0200

    remove link react-router-dom

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import Article from "../components/Article";
 import { handleGetAllArticles } from "../services/handleFunction";
 
